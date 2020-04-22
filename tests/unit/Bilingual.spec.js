61567e1c994a1613a6b9bcddc8882da4767b64e2
tai02 <taito.yokomizo@88oct.co.jp>
2020-04-23 01:02:40 +0900

    feat: add Bilingual-test

---
+++ tests/unit/Bilingual.spec.js
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils";
+import Bilingual from "@/components/Bilingual.vue";
+
+describe("Bilingual", () => {
+  it("renders successfully", () => {
+    const wrapper = mount(Bilingual);
+  });
+});
