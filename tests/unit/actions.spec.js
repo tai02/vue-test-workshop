d4f381dd4c2eee4161647c93d071472c426437c5
tai02 <taito.yokomizo@88oct.co.jp>
2020-04-29 02:33:36 +0900

    test: add testing for the API error

--- tests/unit/actions.spec.js
+++ tests/unit/actions.spec.js
@@ -2,10 +2,13 @@ import actions from "@/store/actions";
 
 let url = "";
 let body = {};
+let mockError = false;
 
 jest.mock("axios", () => ({
   post: (_url, _body) => {
     return new Promise(resolve => {
+      if (mockError) throw Error();
+
       url = _url;
       body = _body;
       resolve(true);
@@ -24,5 +27,13 @@ describe("authenticate", () => {
     expect(url).toBe("/api/authenticate");
     expect(body).toEqual({ username, password });
     expect(commit).toHaveBeenCalledWith("SET_AUTHENTICATED", true);
+  });
+
+  it("catches an error", async () => {
+    mockError = true;
+
+    await expect(
+      actions.authenticate({ commit: jest.fn() }, {})
+    ).rejects.toThrow("API Error occurred.");
   });
 });
