5ab52f275ac2e867adcb535a508203fda423bc5c
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-07 00:30:43 +0900

    test: add test for ComponentWithGetters

---
+++ tests/unit/ComponentWithGetters.spec.js
@@ -0,0 +1,23 @@
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
+import ComponentWithGetters from "@/components/ComponentWithGetters.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    firstName: "Alice",
+    lastName: "Doe"
+  },
+
+  getters: {
+    fullname: state => state.firstName + " " + state.lastName
+  }
+});
+
+it("renders a username using a real Vuex getter", () => {
+  const wrapper = mount(ComponentWithGetters, { store, localVue });
+
+  expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+});
