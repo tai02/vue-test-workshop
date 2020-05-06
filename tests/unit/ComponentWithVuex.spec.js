6262a5a1c6cee27ae3f095e6e3c1a47230225c4f
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-07 00:26:14 +0900

    test: add test with using a mock store

--- tests/unit/ComponentWithVuex.spec.js
+++ tests/unit/ComponentWithVuex.spec.js
@@ -20,4 +20,16 @@ describe("ComponentWithVuex", () => {
 
     expect(wrapper.find(".username").text()).toBe("alice");
   });
+
+  it("renders a username using a mock store", () => {
+    const wrapper = mount(ComponentWithVuex, {
+      mocks: {
+        $store: {
+          state: { username: "alice" }
+        }
+      }
+    });
+
+    expect(wrapper.find(".username").text()).toBe("alice");
+  });
 });
