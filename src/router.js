359bab788120ee53cc54ba970935fb276b96930b
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-11 02:32:02 +0900

    feat: router-hooks

--- src/router.js
+++ src/router.js
@@ -1,7 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./routes.js";
+import { bustCache } from "./bust-cache.js";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({ routes });
+const router = new VueRouter({ routes });
+
+export function beforeEach(to, from, next) {
+  if (to.matched.some(record => record.meta.shouldBustCache)) {
+    bustCache();
+  }
+  next();
+}
+
+router.beforeEach((to, from, next) => beforeEach(to, from, next));
+
+export default router;
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
 import NestedRoute from "@/components/NestedRoute.vue";
 
-export default [{ path: "/nested-route", component: NestedRoute }];
+export default [
+  {
+    path: "/nested-route",
+    component: NestedRoute,
+    meta: {
+      shouldBustCache: true
+    }
+  }
+];
