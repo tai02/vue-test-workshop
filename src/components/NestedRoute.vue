817d6b3a7f0ba948bb33661833a79e1f2c94a52a
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-11 02:05:53 +0900

    feat: create components, router and routes

--- src/components/NestedRoute.vue
+++ src/components/NestedRoute.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div>Nested Route</div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "NestedRoute"
 };
 </script>
