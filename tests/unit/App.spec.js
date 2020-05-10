11406cf656d8d886ea5ce96c6e1bddc147c6098e
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-11 08:52:54 +0900

    refactor: test App.vue with a mocked component

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,11 +1,16 @@
-import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import App from "@/App.vue";
 import VueRouter from "vue-router";
-import NestedRoute from "@/components/NestedRoute.vue";
+// import NestedRoute from "@/components/NestedRoute.vue";
 import routes from "@/routes.js";
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
+
+jest.mock("@/components/NestedRoute.vue", () => ({
+  name: "NestedRoute",
+  render: h => h("div")
+}));
 
 describe("App", () => {
   it("renders a child component via routing", async () => {
@@ -18,7 +23,7 @@ describe("App", () => {
     router.push("/nested-route");
     await wrapper.vm.$nextTick();
 
-    expect(wrapper.find(NestedRoute).exists()).toBe(true);
+    expect(wrapper.find({ name: "NestedRoute" }).exists()).toBe(true);
   });
 
   it("should have a different route that /nested-route", async () => {
@@ -28,7 +33,7 @@ describe("App", () => {
       router
     });
     // This test will fail because we are still on the /nested-route
-    expect(wrapper.find(NestedRoute).exists()).toBe(false);
+    expect(wrapper.find({ name: "NestedRoute" }).exists()).toBe(false);
     // console.log(router.currentRoute);
   });
 });
