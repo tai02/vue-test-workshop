9e2df204c3ad911b7145a23247b696008aab48a2
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-11 23:25:42 +0900

    test: the button click

--- tests/unit/CompositionApi.spec.js
+++ tests/unit/CompositionApi.spec.js
@@ -12,4 +12,15 @@ describe("CompositionApi", () => {
 
     expect(wrapper.find(".message").text()).toBe("TESTING THE COMPOSITION API");
   });
+
+  it("increments a count when button is clicked", async () => {
+    const wrapper = mount(CompositionApi, {
+      propsData: { message: "" }
+    });
+
+    wrapper.find("button").trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find(".count").text()).toBe("Count: 1");
+  });
 });
