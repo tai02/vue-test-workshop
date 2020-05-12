f1f72b34ef2a2f04c196590becfad5c5c5158ffd
tai02 <taito.yokomizo@88oct.co.jp>
2020-05-13 04:01:36 +0900

    refactor: testing with create-wrapper

--- tests/unit/Posts.spec.js
+++ tests/unit/Posts.spec.js
@@ -16,26 +16,28 @@ const createTestVue = () => {
   return { store, router, localVue };
 };
 
+const createWrapper = (component, options = {}) => {
+  const { localVue, store, router } = createTestVue();
+  return mount(component, {
+    store,
+    router,
+    localVue,
+    ...options
+  });
+};
+
 describe("Posts.vue", () => {
   it("renders a message if passed", () => {
-    const { localVue, store, router } = createTestVue();
     const message = "New content coming soon!";
-    const wrapper = mount(Posts, {
-      propsData: { message },
-      store,
-      router,
-      localVue
+    const wrapper = createWrapper(Posts, {
+      propsData: { message }
     });
 
     expect(wrapper.find("#message").text()).toBe("New content coming soon!");
   });
 
   it("renders posts", async () => {
-    const { store, router } = createTestVue();
-    const wrapper = mount(Posts, {
-      store,
-      router
-    });
+    const wrapper = createWrapper(Posts);
 
     wrapper.vm.$store.commit("ADD_POSTS", [{ id: 1, title: "Post" }]);
     await wrapper.vm.$nextTick();
